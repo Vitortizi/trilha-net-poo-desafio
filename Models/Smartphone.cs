29964685d255bd036f82bc101dcf45f923b246d8
vitortizi <vitortizi@gmail.com>
2025-04-20 21:32:32 -0300

    fix: Valição das propriedades privadas e exibição dos dados na mesma.

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -11,10 +11,42 @@ namespace DesafioPOO.Models
         }
 
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-        private string Modelo { get; set; }
-        private string IMEI { get; set; }
-        private int Memoria { get; set; }
+
+        private string _modelo;
+        public string Modelo
+        {
+            get => _modelo;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Modelo não pode ser vazio ou nulo.");
+                _modelo = value;
+            }
+        }
+
+        private string _imei;
+        public string IMEI
+        {
+            get => _imei;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("IMEI não pode ser vazio ou nulo.");
+                _imei = value;
+            }
+        }
+
+        private int _memoria;
+        public int Memoria
+        {
+            get => _memoria;
+            private set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Memória deve ser maior que zero.");
+                _memoria = value;
+            }
+        }
 
         public void Ligar()
         {
--- Program.cs
+++ Program.cs
@@ -3,14 +3,30 @@
 // TODO: Realizar os testes com as classes Nokia e Iphone
 Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456789012345", 32);
 Console.WriteLine("Número do Nokia: " + nokia.Numero);
+Console.WriteLine("Modelo do Nokia: " + nokia.Modelo);
+Console.WriteLine("IMEI do Nokia: " + nokia.IMEI);
+Console.WriteLine("Memória do Nokia: " + nokia.Memoria + "GB");
+
+nokia.Numero = "40028922"; // Alterando o número do Nokia
+Console.WriteLine("Novo número do Nokia: " + nokia.Numero);
+
 nokia.Ligar(); 
 nokia.ReceberLigacao();
 nokia.InstalarAplicativo("WhatsApp"); 
 
-Console.WriteLine(); // Adicionando uma linha em branco para separar as saídas
+
+
+Console.WriteLine("-------------------------------------------------------------");
 
 Iphone iphone = new Iphone("987654321", "iPhone 12", "987654321098765", 128);
 Console.WriteLine("Número do iPhone: " + iphone.Numero);
+Console.WriteLine("Modelo do iPhone: " + iphone.Modelo);
+Console.WriteLine("IMEI do iPhone: " + iphone.IMEI);  
+Console.WriteLine("Memória do iPhone: " + iphone.Memoria + "GB");
+
+iphone.Numero = "89224002"; // Alterando o número do Iphone
+Console.WriteLine("Novo número do Iphone: " + iphone.Numero);
+
 iphone.Ligar();
 iphone.ReceberLigacao();
 iphone.InstalarAplicativo("Instagram");
