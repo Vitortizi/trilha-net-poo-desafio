17f942e59dd476c7fc89851fd9eb9279afe1ed20
vitortizi <vitortizi@gmail.com>
2025-04-20 21:08:20 -0300

    feat: Desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456789012345", 32);
+Console.WriteLine("Número do Nokia: " + nokia.Numero);
+nokia.Ligar(); 
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp"); 
+
+Console.WriteLine(); // Adicionando uma linha em branco para separar as saídas
+
+Iphone iphone = new Iphone("987654321", "iPhone 12", "987654321098765", 128);
+Console.WriteLine("Número do iPhone: " + iphone.Numero);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
